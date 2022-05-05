c180f35c22d30918881f34bff98dfa741723e9c9
ihrubik <ihrubik@frinx.io>
2022-05-05 12:32:42 +0200

    Fifth commit

--- docker/main.py
+++ docker/main.py
@@ -7,25 +7,48 @@ db = {"host": 'db',
       "user": 'postgres',
       "port": 5432}
 
+
 def load_json(json_file):
     with open(json_file) as f:
         data = json.load(f)
-    return data  
+    return data
 
 
 class interface:
-    def __init__(self, name):
+    def __init__(self, name, config, description, mtu):
         self.name = name
+        self.config = config
+        self.description = description
+        self.mtu = mtu
+
+    def attributes(self):
+        return self.name, self.config, self.description, self.mtu
 
 
+frinx = 'frinx-uniconfig-topology:configuration'
+interfaces = "openconfig-interfaces:interfaces"
 
 if __name__ == '__main__':
     data = load_json('configClear_v2.json')
     objects = list()
-    for i in data['frinx-uniconfig-topology:configuration']["openconfig-interfaces:interfaces"]["interface"]:
-        interf = interface(i["name"])
-        print(interf.name)
-        objects.append(interf)
+    # for i in data[frinx]['Cisco-IOS-XE-native:native']['interface']:
+    #     for i in data[frinx]['Cisco-IOS-XE-native:native']['interface'][i]:
+    #         if 'Cisco-IOS-XE-ethernet:channel-group' in i.keys():
+    #             print(i['Cisco-IOS-XE-ethernet:channel-group'])
+
+    for i in data[frinx][interfaces]['interface']:
+        if 'Loopback' not in i['name']:
+            if 'description' not in i['config'].keys():
+                i['config']['description'] = None
+            if 'mtu' not in i['config'].keys():
+                i['config']['mtu'] = None
+            interf = interface(i["name"], i["config"], i['config']['description'],i['config']['mtu'])
+            print(interf.name)
+            print(interf.config)
+            print(interf.description)
+            print(interf.mtu)
+            print()
+            objects.append(interf)
 
     connection = psycopg2.connect(**db)
     cursor = connection.cursor()
@@ -44,12 +67,11 @@ if __name__ == '__main__':
 
     cursor.execute(create_table)
 
-    insert_into_tables_values = 'INSERT INTO interfaces (name) VALUES (%s)'
+    insert_into_tables_values = 'INSERT INTO interfaces (name, config, description, mtu) VALUES (%s %s %s %s)'
 
     for obj in objects:
-        cursor.execute(insert_into_tables_values, (obj.name))
+        cursor.execute(insert_into_tables_values, obj.attributes())
 
     connection.commit()
     cursor.close()
-    connection.close()    
-
+    connection.close()
